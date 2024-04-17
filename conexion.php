bd4658779ecbaa53cf8488330fa01ce268bb2cfb
INOXBEAT <jpreciado85@gmail.com>
2024-04-17 13:01:19 -0500

    mejora en el archivo de la conexion a la base de datos

--- conexion.php
+++ conexion.php
@@ -1,8 +1,19 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "", "sistema");
+$host = "localhost";
+$usuario = "root";
+$contrasena = "";
+$base_de_datos = "sistema";
+$charset = "utf8mb4"; 
 
-if (!$mysqli) {
-    die("Error de conexión: " . mysqli_connect_error());
+$mysqli = new mysqli($host, $usuario, $contrasena, $base_de_datos);
+
+if ($mysqli->connect_errno) {
+    throw new Exception("Error de conexión: " . $mysqli->connect_error);
 }
+
+if (!$mysqli->set_charset($charset)) {
+    throw new Exception("Error al configurar el conjunto de caracteres: " . $mysqli->error);
+}
+
 ?>
